d3853966dde709ddfe73b26ba271b6cf1fe732e2
dutarrr <duzguntutar@gmail.com>
2021-10-07 19:38:35 +0300

    ui react

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,16 +1,33 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch ,Link} from "react-router-dom";
 import Home from "./pages/Home";
-import Post from "./pages/Post";
+import Posts from "./components/Posts";
+import Categories from "./components/Categories"
 
 function App() {
   return (
     <div className="container">
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/post" component={Post} />
-        </Switch>
+      <div className="row mt-2">
+        <div className="col-md-4">
+          <ul className="list-group">
+            <li className="list-group-item "><Link to="/"> Home </Link></li>
+            <li className="list-group-item "><Link to="/post"> Post </Link></li>
+            <li className="list-group-item"><Link to="/category">Category</Link></li>
+          </ul>
+
+        </div>
+        <div className="col-md-8">
+          
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/post" component={Posts} />
+              <Route exact path="/category" component={Categories} />
+
+            </Switch>
+          
+        </div>
+      </div>
       </Router>
     </div>
   );
